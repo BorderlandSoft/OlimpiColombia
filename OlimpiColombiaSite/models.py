1251c0dfe6366c6207022289b24448f5fd88c10e
jcastrov <johcavi@gmail.com>
2016-08-15 18:03:02 -0500

    Se agregó el Modelo Deporte

--- OlimpiColombiaSite/models.py
+++ OlimpiColombiaSite/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+
+class Deporte(models.Model):
+    nombre = models.CharField(max_length=200)
+    imagen = models.CharField(max_length=1000)
+
+    def __str__(self):
+        return self.nombre
