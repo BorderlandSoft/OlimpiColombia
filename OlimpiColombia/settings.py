b24ff9e4fccd7377f435eedd09ae530b715e68ae
jcastrov <johcavi@gmail.com>
2016-08-15 17:52:12 -0500

    deportes.html agregado

--- OlimpiColombia/settings.py
+++ OlimpiColombia/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = ['*']
 # Application definition
 
 INSTALLED_APPS = [
+    'OlimpiColombiaSite.apps.OlimpicolombiasiteConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
