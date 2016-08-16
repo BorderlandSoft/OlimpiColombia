03bea687d323e0b8c8c94ae58efb7af1d9be0641
jwsanabria <jwsanabria@gmail.com>
2016-08-15 21:00:13 -0500

    Se agregan migraciones generadas para replicar el modelo y carga inicial de datos en la entidad deporte

--- OlimpiColombiaSite/models.py
+++ OlimpiColombiaSite/models.py
@@ -8,23 +8,22 @@ class Imagen(models.Model):
 
 class Deporte(models.Model):
 	nombre = models.CharField(max_length=200)
-	descripcion = models.CharField(max_length=150)
+	descripcion = models.CharField(max_length=150, blank=True)
 	imagen =  models.CharField(max_length=1000)
-	archivo = models.ImageField(upload_to='imagenes', null=true)
 
 class Atleta(models.Model):
     nombres = models.CharField(max_length=200)
     lugar_nacimiento = models.CharField(max_length=100)
     fecha_nacimiento = models.DateField()
-    edad = models.IntegerField(max_length=2)
+    edad = models.IntegerField()
     peso = models.DecimalField(max_digits=4, decimal_places=1)
     estatura = models.DecimalField(max_digits=4, decimal_places=1)
-    fotografia = models.IntegerField(max_length=1000)
-    video_destacado = models.CharField(max_length=1000, null=true)
+    fotografia = models.CharField(max_length=1000)
+    video_destacado = models.CharField(max_length=1000, blank=True)
 
 class Modalidad(models.Model):
 	nombre = models.CharField(max_length=50)
-	medalla = models.CharField(max_length=20, blank=true)
+	medalla = models.CharField(max_length=20, blank=True)
 	entrenador = models.CharField(max_length=200)
 	deporte = models.ForeignKey(Deporte, on_delete=models.CASCADE)
 	atleta = models.ForeignKey(Atleta, on_delete=models.CASCADE)
@@ -32,9 +31,9 @@ class Modalidad(models.Model):
 class Competencia(models.Model):
     fecha = models.DateField()
     hora = models.TimeField()
-    rival = models.CharField(max_length=100, blank=true)
+    rival = models.CharField(max_length=100, blank=True)
     resultado = models.CharField(max_length=100)
-    video_destacado = models.CharField(max_length=1000, blank=true)
+    video_destacado = models.CharField(max_length=1000, blank=True)
     modalidad = models.ForeignKey(Modalidad, on_delete=models.CASCADE)
 
     def __str__(self):
