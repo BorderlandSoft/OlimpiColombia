4a71423213031e9985aa652968df01ea4bf43114
jcastrov <johcavi@gmail.com>
2016-08-16 00:30:53 -0500

    Limite de la descripción aumentado a 1000 caracteres

--- OlimpiColombiaSite/models.py
+++ OlimpiColombiaSite/models.py
@@ -2,14 +2,17 @@ from __future__ import unicode_literals
 
 from django.db import models
 
+
 # Create your models here.
 class Imagen(models.Model):
     url = models.CharField(max_length=1000)
 
+
 class Deporte(models.Model):
-	nombre = models.CharField(max_length=200)
-	descripcion = models.CharField(max_length=150, blank=True)
-	imagen =  models.CharField(max_length=1000)
+    nombre = models.CharField(max_length=200)
+    descripcion = models.CharField(max_length=1000, blank=True)
+    imagen = models.CharField(max_length=1000)
+
 
 class Atleta(models.Model):
     nombres = models.CharField(max_length=200)
@@ -21,12 +24,14 @@ class Atleta(models.Model):
     fotografia = models.CharField(max_length=1000)
     video_destacado = models.CharField(max_length=1000, blank=True)
 
+
 class Modalidad(models.Model):
-	nombre = models.CharField(max_length=50)
-	medalla = models.CharField(max_length=20, blank=True)
-	entrenador = models.CharField(max_length=200)
-	deporte = models.ForeignKey(Deporte, on_delete=models.CASCADE)
-	atleta = models.ForeignKey(Atleta, on_delete=models.CASCADE)
+    nombre = models.CharField(max_length=50)
+    medalla = models.CharField(max_length=20, blank=True)
+    entrenador = models.CharField(max_length=200)
+    deporte = models.ForeignKey(Deporte, on_delete=models.CASCADE)
+    atleta = models.ForeignKey(Atleta, on_delete=models.CASCADE)
+
 
 class Competencia(models.Model):
     fecha = models.DateField()
