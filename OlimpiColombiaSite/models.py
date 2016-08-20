7c4b0c1ef280dc6d8b622e466bfa7894910fe579
Alejandro Espinosa <alejosp493@gmail.com>
2016-08-20 16:58:37 -0500

    Edición de modelo y documenntación
    Modelos documentados y editados campos del modelo en atleta y
    competencia.

--- OlimpiColombiaSite/models.py
+++ OlimpiColombiaSite/models.py
@@ -7,38 +7,46 @@ from django.db import models
 class Imagen(models.Model):
     url = models.CharField(max_length=1000)
 
-
 class Deporte(models.Model):
+    """
+    Describe un deporte.
+    """
     nombre = models.CharField(max_length=200)
     descripcion = models.CharField(max_length=1000, blank=True)
     imagen = models.CharField(max_length=1000)
 
-
 class Atleta(models.Model):
+    """
+    Describe a un Atleta.
+    """
     nombres = models.CharField(max_length=200)
     lugar_nacimiento = models.CharField(max_length=100)
     fecha_nacimiento = models.DateField()
     edad = models.IntegerField()
     peso = models.DecimalField(max_digits=4, decimal_places=1)
     estatura = models.DecimalField(max_digits=4, decimal_places=1)
-    fotografia = models.CharField(max_length=1000)
+    fotografia = models.ImageField(max_length=1000)
     video_destacado = models.CharField(max_length=1000, blank=True)
 
-
 class Modalidad(models.Model):
+    """
+    Defina la modalidad en la que participa un atleta para un deporte
+    y quien es su entrenador.
+    """
     nombre = models.CharField(max_length=50)
     medalla = models.CharField(max_length=20, blank=True)
     entrenador = models.CharField(max_length=200)
     deporte = models.ForeignKey(Deporte, on_delete=models.CASCADE)
     atleta = models.ForeignKey(Atleta, on_delete=models.CASCADE)
 
-
 class Competencia(models.Model):
+    """
+    Describe una competencia para un deporte.
+    """
     fecha = models.DateField()
     hora = models.TimeField()
-    rival = models.CharField(max_length=100, blank=True)
+    participantes = models.ManyToManyField(Atleta)
     resultado = models.CharField(max_length=100)
-    video_destacado = models.CharField(max_length=1000, blank=True)
     modalidad = models.ForeignKey(Modalidad, on_delete=models.CASCADE)
 
     def __str__(self):
