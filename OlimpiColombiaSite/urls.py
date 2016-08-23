eff1cf8f39fb329ce52ed25bdc88b23c5ac089b0
Alejandro Espinosa <alejosp493@gmail.com>
2016-08-23 14:43:51 -0500

    fixes
    correcciones de fallas

--- OlimpiColombiaSite/urls.py
+++ OlimpiColombiaSite/urls.py
@@ -6,6 +6,6 @@ app_name = 'OlimpiColombiaSite'
 urlpatterns = [
     url(r'^$', views.IndexView.as_view(), name='index'),
     url(r'^atletas/(?P<atleta_id>[0-9]+)/$', views.AtletasView.as_view(), name='atleta'),
-    url(r'^highlights/(?P<pk>[0-9]+)/$', views.AtletasView.as_view(), name='atleta'),
+    url(r'^highlights/(?P<pk>[0-9]+)/$', views.HighlightView.as_view(), name='atleta'),
     url(r'^calendario/(?P<pk>\d+)/$', views.CalendarioView.as_view()),
 ]
