f58718d344ba5106e02aaf8e64dcd7bc10fa4d38
Alejandro Espinosa <alejosp493@gmail.com>
2016-08-23 23:33:35 -0500

    fix

--- OlimpiColombiaSite/urls.py
+++ OlimpiColombiaSite/urls.py
@@ -8,5 +8,4 @@ urlpatterns = [
     url(r'^atletas/(?P<atleta_id>[0-9]+)/$', views.AtletasView.as_view(), name='atleta'),
     url(r'^highlights/(?P<pk>[0-9]+)/$', views.HighlightView.as_view(), name='atleta'),
     url(r'^calendario/(?P<pk>\d+)/$', views.CalendarioView.as_view()),
-    static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT),
 ]
