2a81e38b5d37c5407c4344330fd984e71c7ac4c6
Alejandro Espinosa <alejosp493@gmail.com>
2016-08-23 23:42:42 -0500

    typo

--- OlimpiColombia/urls.py
+++ OlimpiColombia/urls.py
@@ -15,7 +15,7 @@ Including another URLconf
 """
 from django.conf.urls import include, url
 from django.contrib import admin
-from . import setting
+from . import settings
 
 urlpatterns = [
     url(r'', include('OlimpiColombiaSite.urls')),
