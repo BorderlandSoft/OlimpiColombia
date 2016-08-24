63c6d2ff9e749ae9bb966741f58181f1d51dbf1f
Alejandro Espinosa <alejosp493@gmail.com>
2016-08-23 23:28:07 -0500

    servir archivos estáticos

--- OlimpiColombiaSite/urls.py
+++ OlimpiColombiaSite/urls.py
@@ -1,6 +1,6 @@
 from django.conf.urls import url
 
-from . import views
+from . import views, settings
 
 app_name = 'OlimpiColombiaSite'
 urlpatterns = [
@@ -8,4 +8,5 @@ urlpatterns = [
     url(r'^atletas/(?P<atleta_id>[0-9]+)/$', views.AtletasView.as_view(), name='atleta'),
     url(r'^highlights/(?P<pk>[0-9]+)/$', views.HighlightView.as_view(), name='atleta'),
     url(r'^calendario/(?P<pk>\d+)/$', views.CalendarioView.as_view()),
+    static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT),
 ]
